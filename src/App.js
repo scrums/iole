82cd5f8aac59aaaba9eb077f2309b1eb89cef0b1
wariorx <rcalcamo@uncc.edu>
2017-10-29 10:55:43 -0400

    will this work?

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import rd3 from 'react-d3-library';
+import node from 'd3file';
+const RD3Component = rd3.Component;
 
 class App extends Component {
   render() {
@@ -13,6 +16,52 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <div className = "Description">
+        </div>
+        <div classNamme = "Graph">
+        var node = document.createElement('div');
+        var diameter = 960,
+          format = d3.format(",d"),
+          color = d3.scale.category20c();
+
+        var bubble = d3.layout.pack()
+          .sort(null)
+          .size([diameter, diameter])
+          .padding(1.5);
+
+        var svg = d3.select('node').append("svg")
+            .attr("width", diameter)
+            .attr("height", diameter)
+            .attr("class", "bubble");
+
+        d3.json("flare.json", function(error, root) {
+          if (error) throw error;
+
+        var bubbles = svg.selectAll(".node")
+            .data(bubble.nodes(classes(flare))
+            .filter(function(d) { return !d.children; }))
+            .enter().append("g")
+            .attr("class", "node")
+            .attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+
+            class my_First_React_D3_Library_Component extends React.Component {
+
+            constructor(props) {
+              super(props);
+              this.state = {d3: ''}
+            }
+
+            componentDidMount() {
+              this.setState({d3: node});
+            }
+
+            render() {
+              return (
+                <div>
+                  <RD3Component data={this.state.d3} />
+                </div>
+              )
+            }
       </div>
     );
   }
